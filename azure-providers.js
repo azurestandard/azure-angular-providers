f3046296fa59a439ae140932bf7d2417232e35b4
W. Trevor King <wking@tremily.us>
2015-01-07 11:03:01 -0800

    azure-providers.js: Add AzureCarts for managing order creation
    Users can have several in-progress carts, and we use the cart
    information a lot.  This commit adds a factory that tracks a person's
    carts using a Carts object.  The factory has an internal cart_sets
    variable to keep references to any loaded carts, so multiple calls to:
    
      AzureCarts(person_id)
    
    will all return the same Carts instance.  That makes it easy to share
    state between several controllers and directives, and lets us avoid
    the additional network requests that you'd get if each controller
    loaded it's own cart data directly.  It also makes it easy to share
    information about the currently-selected cart.
    
    The goal is to have the next-to-cutoff cart be the default cart (this
    commit just uses .carts[0]).  We store the currently-selected cart in
    Carts.cart, and you can change the selection with
    Carts.selectCart(order_id), since Benjamin doesn't want it to persist
    between refreshes.  I'd consider having it persist between refreshes
    and be bound to the current session, but then we'd need more API
    endpoints to set that for the session.

--- azure-providers.js
+++ azure-providers.js
@@ -59,4 +59,79 @@ var azureProvidersModule = angular
             });
             return resources;
         }]
-    });
+    })
+    .factory('AzureCarts', ['AzureAPI', function AzureCartsFactory(AzureAPI) {
+        var cart_sets = {};
+
+        var Cart = function(order) {
+            var _this = this;
+            this.order = order;
+            if (order.drop) {
+                this.drop = AzureAPI.drop.get({
+                    id: order.drop,
+                });
+            }
+            if (order.trip) {
+                this.trip = AzureAPI.trip.get({
+                    id: order.trip,
+                });
+            }
+            this.orderLines = AzureAPI['order-line'].query({
+                order: order.id,
+            });
+            this.price = 0;
+            this.weight = 0;
+            this.products = 0;
+            this.orderLines.$promise.then(function(lines) {
+                lines.forEach(function(line) {
+                    if (line.price['per-pound']) {
+                        line.totalPrice = line.price.dollars * line.weight;
+                    } else {
+                        line.totalPrice =
+                            line.price.dollars * line['quantity-ordered'];
+                    }
+                    _this.price += line.totalPrice;
+                    _this.weight += line.weight;
+                    _this.products += line['quantity-ordered'];
+                    line.productObject = AzureAPI.product.get({
+                        code: line.product,
+                    });
+                });
+            });
+        };
+
+        var Carts = function(person_id) {
+            var _this = this;
+            this.carts = [];
+            this.cart = null;
+            var orders = AzureAPI.order.query({
+                person: person_id,
+                status: 'cart',
+            });
+            orders.$promise.then(function(orders) {
+                orders.forEach(function(order) {
+                    _this.carts.push(new Cart(order));
+                });
+                _this.cart = _this.carts[0];  // TODO: pick next-to-cutoff cart
+            });
+        };
+
+        Carts.prototype.selectCart = function(order_id) {
+            var match = this.carts.some(function(cart) {
+                if (cart.order.id === order_id) {
+                    return true;
+                }
+            });
+            if (!match) {
+                console.log('no match found for ' + order_id + ' in', this.carts);
+            }
+        };
+
+        return function(person_id) {
+            if (!cart_sets.hasOwnProperty(person_id)) {
+                /* we don't have an existing instance */
+                cart_sets[person_id] = new Carts(person_id);
+            }
+            return cart_sets[person_id];
+        };
+    }]);
