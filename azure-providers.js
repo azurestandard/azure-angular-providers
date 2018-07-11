287be742ccbb1def00e173dea3206522296e9f59
David McAtee <software4@azurestandard.com>
2018-07-11 12:10:04 -0700

    Add packaged-product old action
    Also reduces nesting and formats file

--- azure-providers.js
+++ azure-providers.js
@@ -13,8 +13,8 @@
         .constant('AzureModelIdentifiers', {
             'bartender-host': 'code',
             'packaged-product': 'code',
-            'route': 'name',
-            'warehouse': 'code',
+            route: 'name',
+            warehouse: 'code'
         })
         .provider('AzureAPI', AzureAPIProvider);
 
@@ -54,46 +54,46 @@
             'route-stop',
             'stop',
             'trip',
-            'warehouse',
+            'warehouse'
         ];
         var _plurals = {
             'account-entry': 'account-entries',
-            'address': 'addresses',
-            'category': 'categories',
-            'country': 'countries',
-            'person': 'people',
+            address: 'addresses',
+            category: 'categories',
+            country: 'countries',
+            person: 'people'
         };
 
         // non-$resource endpoints that will use $http.post(...)
         var _posts = {
             login: {
                 url: '/login',
-                withCredentials: true,
+                withCredentials: true
             },
             logout: {
                 url: '/logout',
-                withCredentials: true,
+                withCredentials: true
             },
             register: {
                 url: '/registration/register',
-                withCredentials: true,
+                withCredentials: true
             },
             resendRegistrationEmail: {
-                url: '/registration/resend',
+                url: '/registration/resend'
             },
             resetPassword: {
-                url: '/password/reset',
+                url: '/password/reset'
             },
             resetPasswordConfirm: {
-                url: '/password/confirm',
-            },
+                url: '/password/confirm'
+            }
         };
 
         var _headers = {
-            'Accept': 'application/json',
+            Accept: 'application/json'
         };
         var _payloadHeaders = {
-            'Content-Type': 'application/json; charset=UTF-8',
+            'Content-Type': 'application/json; charset=UTF-8'
         };
         var _apiUrl = 'https://api.azurestandard.com';
 
@@ -101,7 +101,9 @@
             _apiUrl = value;
         };
 
-        this.$get = ['$http', '$resource', 'AzureModelIdentifiers', function AzureAPIFactory($http, $resource, AzureModelIdentifiers) {
+        this.$get = ['$http', '$resource', 'AzureModelIdentifiers', AzureAPIFactory];
+
+        function AzureAPIFactory($http, $resource, AzureModelIdentifiers) {
             var payloadHeaders = {};
             for (var header in _headers) {
                 payloadHeaders[header] = _headers[header];
@@ -111,11 +113,13 @@
             }
             var resources = {
                 session: $resource(
-                    _apiUrl + '/session', {}, {
+                    _apiUrl + '/session',
+                    {},
+                    {
                         get: {
                             method: 'GET',
                             withCredentials: true,
-                            headers: _headers,
+                            headers: _headers
                         }
                     }
                 )
@@ -124,7 +128,7 @@
             for (var name in _posts) {
                 var postConfig = _posts[name];
                 var config = {
-                    headers: payloadHeaders,
+                    headers: payloadHeaders
                 };
                 if (postConfig.withCredentials) {
                     config.withCredentials = true;
@@ -142,67 +146,67 @@
                         url: _apiUrl + '/' + plural,
                         isArray: true,
                         withCredentials: true,
-                        headers: _headers,
+                        headers: _headers
                     },
                     count: {
                         method: 'HEAD',
                         url: _apiUrl + '/' + plural,
                         params: {
-                            limit: 0,
+                            limit: 0
                         },
                         withCredentials: true,
                         headers: _headers,
                         interceptor: {
                             response: function(response) {
                                 response.resource.count = parseInt(
-                                    response.headers('Count'));
+                                    response.headers('Count')
+                                );
                                 return response;
-                            },
-                        },
+                            }
+                        }
                     },
                     create: {
                         method: 'POST',
                         url: _apiUrl + '/' + plural,
                         withCredentials: true,
-                        headers: payloadHeaders,
+                        headers: payloadHeaders
                     },
                     get: {
                         method: 'GET',
                         withCredentials: true,
-                        headers: _headers,
+                        headers: _headers
                     },
                     save: {
                         method: 'PUT',
                         withCredentials: true,
-                        headers: payloadHeaders,
+                        headers: payloadHeaders
                     },
-                    'delete': {
+                    delete: {
                         method: 'DELETE',
                         withCredentials: true,
-                        headers: _headers,
-                    },
+                        headers: _headers
+                    }
                 };
                 if (['person', 'route', 'trip'].indexOf(model) !== -1) {
                     actions.mail = {
                         method: 'POST',
                         url: _apiUrl + '/mail/' + model + '/:' + identifier,
                         withCredentials: true,
-                        headers: payloadHeaders,
+                        headers: payloadHeaders
                     };
                     actions.mails = {
                         method: 'POST',
                         url: _apiUrl + '/mail/' + plural,
                         withCredentials: true,
-                        headers: payloadHeaders,
+                        headers: payloadHeaders
                     };
-                }
-                if (model === 'audit-product') {
+                } else if (model === 'audit-product') {
                     actions = {
                         get: {
                             method: 'GET',
                             url: _apiUrl + '/audit/product/' + ':' + identifier,
                             withCredentials: true,
-                            headers: _headers,
+                            headers: _headers
                         },
                         query: {
                             method: 'GET',
@@ -213,61 +217,83 @@
                             interceptor: {
                                 response: function(response) {
                                     response.resource.count = parseInt(
-                                        response.headers('Count'));
+                                        response.headers('Count')
+                                    );
                                     return response;
-                                },
-                            },
+                                }
+                            }
                         },
                         save: {
                             method: 'POST',
                             url: _apiUrl + '/audit/packaged-product/' + ':' + identifier,
                             withCredentials: true,
-                            headers: payloadHeaders,
-                        },
+                            headers: payloadHeaders
+                        }
                     };
-                }
-                if (model === 'order') {
+                } else if (model === 'order') {
                     actions.parcelCarrierEstimates = {
                         method: 'GET',
-                        url: _apiUrl + '/order/:' + identifier + '/parcel-carrier-fee-estimates',
+                        url:
+                            _apiUrl +
+                            '/order/:' +
+                            identifier +
+                            '/parcel-carrier-fee-estimates',
                         isArray: true,
                         withCredentials: true,
                         headers: _headers
                     };
-                }
-                if (model === 'packaged-product') {
-                    var categoryUrl = _apiUrl + '/' + model + '/:' + identifier + '/category/:categoryId';
+                } else if (model === 'packaged-product') {
+                    var categoryUrl =
+                        _apiUrl +
+                        '/' +
+                        model +
+                        '/:' +
+                        identifier +
+                        '/category/:categoryId';
                     var params = {
-                        'categoryId': '@categoryId'
+                        categoryId: '@categoryId'
                     };
                     actions.addCategory = {
                         method: 'POST',
                         url: categoryUrl,
                         params: params,
                         withCredentials: true,
-                        headers: _headers,
+                        headers: _headers
                     };
                     actions.removeCategory = {
                         method: 'DELETE',
                         url: categoryUrl,
                         params: params,
                         withCredentials: true,
-                        headers: _headers,
+                        headers: _headers
                     };
-                }
-                if (model === 'drop') {
+                    actions.old = {
+                        method: 'GET',
+                        url: _apiUrl + '/' + model + '/old/:id',
+                        params: {
+                            id: '@id'
+                        },
+                        withCredentials: true,
+                        headers: _headers
+                    };
+                } else if (model === 'drop') {
                     actions.locations = {
                         method: 'GET',
                         url: _apiUrl + '/' + plural + '/locations',
                         isArray: true,
                         withCredentials: true,
-                        headers: _headers,
+                        headers: _headers
                     };
-                }
-                if (model === 'bartender-print-configuration') {
+                } else if (model === 'bartender-print-configuration') {
                     actions.testPrint = {
                         method: 'POST',
-                        url: _apiUrl + '/' + model + '/:' + identifier + '/actions/test-print',
+                        url:
+                            _apiUrl +
+                            '/' +
+                            model +
+                            '/:' +
+                            identifier +
+                            '/actions/test-print',
                         withCredentials: true,
                         headers: _headers
                     };
@@ -283,17 +309,13 @@
 
             function createPost(postConfig, config) {
                 return function(data) {
-                    return $http.post(
-                        _apiUrl + postConfig.url,
-                        data,
-                        config
-                    );
+                    return $http.post(_apiUrl + postConfig.url, data, config);
                 };
             }
 
             //================================================================================
 
             return resources;
-        }];
+        }
     }
 })(angular);
